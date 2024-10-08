8b4f1b13ee09c721c938ea1396d07c8b9ba8597a
Kitsune <tekohuemmanuel@gmail.com>
2024-10-08 13:18:18 -1000

    Initialisation

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php
+require 'lib/autoloader.php';
+use teachframe\Router;
+use teachframe\PDOWrapper;
+
+PDOWrapper::setConfig('config.yaml');
+
+$router = new Router();
+$router->loadRoutes('routes.yaml');
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING']);
