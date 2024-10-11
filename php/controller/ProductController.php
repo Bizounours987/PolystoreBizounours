7032766c12282a7e7cb9b0a8d32c0c5d172695f1
Kagami <kagamiaisu@gmail.com>
2024-10-11 11:28:01 -1000

    Test echo de ProductController et ProductView

--- php/controller/ProductController.php
+++ php/controller/ProductController.php
@@ -1,5 +1,24 @@
 <?php
 namespace controller;
 
+use \view\ProductView;
+use teachframe;
+
+
 class ProductController {
+
+    function getAll() {
+        
+        echo "Controller Product getAll\n";
+    
+    }
+
+
+    function getOne() {
+        
+        echo "Controller Product getOne\n";
+    
+    }
+
+
 }
--- php/view/ProductView.php
+++ php/view/ProductView.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace view;
+
+use \model\Product;
+use \controller\ProductCOntroller;
+use teachframe;
+
+
+class ProductView {
+
+    public static function getAll() {
+        
+        echo "View Product getAll\n";
+    }
+
+
+    public static  function getOne() {
+    
+        echo "View Product getOne\n";
+    }
+
+}
