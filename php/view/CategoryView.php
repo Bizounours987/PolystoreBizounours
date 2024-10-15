42865658f990fa95f95c1a92737fc3423cb5dee5
Kitsune <tekohuemmanuel@gmail.com>
2024-10-15 08:00:51 -1000

    Creation de la fonction renderOne()

--- php/view/CategoryView.php
+++ php/view/CategoryView.php
@@ -3,6 +3,7 @@
 namespace view;
 
 class CategoryView {
+   
     public function renderAll(array $items){
         echo "<ul>";
         foreach($items as $item) {
@@ -10,5 +11,17 @@ class CategoryView {
         }
         echo "</ul>";
     }
+    public function renderOne($item){
+        if ($item){
+            echo "<h2>Détails</h2>";
+            echo "<p>ID : ".$item->getId(). "</p>";
+            echo "<p>Label: ".$item->getLabel(). "</p>";
+
+        } else{
+            echo "Catégorie non trouvé";
+        }
+    }
+    
+
 }
 
