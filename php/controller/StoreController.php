86831e651a3f42a5783634657ca54346ff732a0b
KayakZaki <v.rouge2507@gmail.com>
2024-10-15 08:01:57 -1000

    Suppression des images

--- php/controller/StoreController.php
+++ php/controller/StoreController.php
@@ -1,1 +1,20 @@
+<?php
+namespace controller;
+
+use model\StoreCollection;
+
+class StoreController {
+
+    // Fonction pour afficher la page des boutiques
+    public function displayStores() {
+        // Création d'une collection de boutiques
+        $storeCollection = new StoreCollection();
+
+        // Supposons que la méthode getStores() renvoie une liste des boutiques disponibles
+        $stores = $storeCollection->getStores();
+
+        // Afficher la vue (index.html par exemple)
+        include __DIR__ . '/../view/index.php';
+    }
+}
 
--- php/index.php
+++ php/index.php
@@ -8,3 +8,5 @@ PDOWrapper::setConfig('config.yaml');
 $router = new Router();
 $router->loadRoutes('routes.yaml');
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['QUERY_STRING']);
+
+
